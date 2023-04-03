f8fc9813d1ebea8f2c894639c115b5ea56e1311f
k0n4nd484r84r14n@gmail.com <alara@theworkshop.com>
2023-04-03 16:11:32 +0200

    test

---
+++ ApplicationTest.java
@@ -0,0 +1,16 @@
+src/test/java/clinic/programming/training/ApplicationTest.java|3 error| package org.junit does not exist
+src/test/java/clinic/programming/training/ApplicationTest.java|4 error| package org.junit does not exist
+src/test/java/clinic/programming/training/ApplicationTest.java|5 error| package org.junit does not exist
+src/test/java/clinic/programming/training/ApplicationTest.java|6 error| package org.junit does not exist
+src/test/java/clinic/programming/training/ApplicationTest.java|11 error| cannot find symbol
+src/test/java/clinic/programming/training/ApplicationTest.java|17 error| cannot find symbol
+src/test/java/clinic/programming/training/ApplicationTest.java|23 error| cannot find symbol
+src/test/java/clinic/programming/training/ApplicationTest.java|29 error| cannot find symbol
+src/test/java/clinic/programming/training/ApplicationTest.java|35 error| cannot find symbol
+src/test/java/clinic/programming/training/ApplicationTest.java|36 error| cannot find symbol
+src/test/java/clinic/programming/training/ApplicationTest.java|42 error| cannot find symbol
+src/test/java/clinic/programming/training/ApplicationTest.java|20 error| cannot find symbol
+src/test/java/clinic/programming/training/ApplicationTest.java|26 error| cannot find symbol
+src/test/java/clinic/programming/training/ApplicationTest.java|32 error| cannot find symbol
+src/test/java/clinic/programming/training/ApplicationTest.java|39 error| cannot find symbol
+src/test/java/clinic/programming/training/ApplicationTest.java|45 error| cannot find symbol
